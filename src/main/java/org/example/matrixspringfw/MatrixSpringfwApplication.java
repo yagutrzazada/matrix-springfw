acc0a2aae54422c7ccdcb98aa109aab2c4aec239
yagutrzazada <yaqutrzazade@gmail.com>
2025-03-15 01:40:45 +0400

    first commit

---
+++ src/main/java/org/example/matrixspringfw/MatrixSpringfwApplication.java
@@ -0,0 +1,13 @@
+package org.example.matrixspringfw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatrixSpringfwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatrixSpringfwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/matrixspringfw/MatrixSpringfwApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.matrixspringfw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatrixSpringfwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
